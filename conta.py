153f9cf5a476dba61a7241c2307048d941f7d672
Pedro Gonzaga <pedro.gonzaga@deucredito.com.br>
2022-06-07 01:29:21 -0300

    100% do curso 3

--- conta.py
+++ conta.py
@@ -1,23 +1,53 @@
-
-
 class Conta:
 
     # Função construtora
     
     def __init__(self, numero, titular, saldo, limite):
         print(f"Construindo objeto ...{self}")
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     # Métodos
 
     def extrato(self):
-        print("O Saldo de {} é {} reais.".format(self.titular, self.saldo))
+        print("O Saldo de {} é {} reais.".format(self.__titular, self.__saldo))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel
 
     def saca(self, valor):
-        self.saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print(f"O valor de {valor} reais passou o limite.")
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def limite(self):
+        return self.__limite
+    
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite 
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+        
+    @staticmethod
+    def codigo_bancos():
+        return "{'BB': '001', 'Caixa': '104', 'Bradesco':'237'}"
--- teste.py
+++ teste.py
@@ -1,12 +1,24 @@
-def cria_conta(numero, titular, saldo, limite):
-    conta = {"numero": numero, "titular": titular, "saldo":saldo, "limite": limite}
-    return conta
+# def cria_conta(numero, titular, saldo, limite):
+#     conta = {"numero": numero, "titular": titular, "saldo":saldo, "limite": limite}
+#     return conta
 
-def deposita(conta, valor):
-    conta["saldo"] += valor
+# def deposita(conta, valor):
+#     conta["saldo"] += valor
 
-def saca(conta, valor):
-    conta["saldo"] -= valor
+# def saca(conta, valor):
+#     conta["saldo"] -= valor
 
-def extrato(conta):
-    print(f"Saldo é {conta['saldo']}")
+# def extrato(conta):
+#     print(f"Saldo é {conta['saldo']}")
+
+class Retangulo:
+
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+        self.__area = x * y
+
+    def obter_area(self):
+        return self.__area
+
+        
