94af95b17fd099c3b741101f4ae815a7451a9841
Pedro Gonzaga <pedro.gonzaga@deucredito.com.br>
2022-06-14 00:17:19 -0300

    curso string methods 56%

---
+++ strings/main.py
@@ -0,0 +1,33 @@
+# url = "https://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=1000"
+
+url = " "
+
+# Sanitização da URL
+
+url = url.strip()
+
+# Validação da URL
+
+
+if url == "":
+    raise ValueError("A URL está vazia.")
+
+# Separando base e parametros
+indice_interregocao = url.find('?')
+url_base = url[:indice_interregocao]
+url_parametros = url[indice_interregocao+1:]
+print(url_parametros)
+
+
+# Buscando valores dos parâmetros
+parametro_busca = 'quantidade'
+indice_parametro = url_parametros.find(parametro_busca)
+
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+
+print(valor)
